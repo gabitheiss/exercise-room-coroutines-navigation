c41a2d75f12d1a21383070bd7478670925a48c99
gabitheiss <t_gaaby@hotmail.com>
2021-09-20 17:12:44 -0300

    Dao insert

--- app/src/main/java/com/example/exercise_room_coroutines/dao/ProductsDAO.kt
+++ app/src/main/java/com/example/exercise_room_coroutines/dao/ProductsDAO.kt
@@ -1,6 +1,8 @@
 package com.example.exercise_room_coroutines.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.exercise_room_coroutines.model.Products
 
@@ -11,4 +13,9 @@ interface ProductsDAO {
     @Query("SELECT * FROM Products ORDER BY make_name")
     fun all() : List<Products>
 
+
+    //insert para inserir no banco
+    //onConflict server para validar quando o insert identifica algum resigtro duplicado
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(products: Products)
 }
