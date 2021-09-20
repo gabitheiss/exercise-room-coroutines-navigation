a07280de3cbde6542be2c56dd08de7315c45965d
gabitheiss <t_gaaby@hotmail.com>
2021-09-20 17:09:04 -0300

    Dao

--- app/src/main/java/com/example/exercise_room_coroutines/dao/ProductsDAO.kt
+++ app/src/main/java/com/example/exercise_room_coroutines/dao/ProductsDAO.kt
@@ -1,5 +1,14 @@
 package com.example.exercise_room_coroutines.dao
 
+import androidx.room.Dao
+import androidx.room.Query
+import com.example.exercise_room_coroutines.model.Products
 
+@Dao
 interface ProductsDAO {
+
+    //select para retornar todos os produtos
+    @Query("SELECT * FROM Products ORDER BY make_name")
+    fun all() : List<Products>
+
 }
