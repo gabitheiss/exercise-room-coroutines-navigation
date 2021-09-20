531509b7e4792afda86adfdc089c2b8b3dd37dd7
gabitheiss <t_gaaby@hotmail.com>
2021-09-20 17:06:28 -0300

    Entity

--- app/src/main/java/com/example/exercise_room_coroutines/model/Products.kt
+++ app/src/main/java/com/example/exercise_room_coroutines/model/Products.kt
@@ -1,12 +1,21 @@
 package com.example.exercise_room_coroutines.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
+@Entity
 class Products(
 
-
+    @PrimaryKey
+    @ColumnInfo(name = "make_name")
     @SerializedName("name") val nome: String,
+
+    @ColumnInfo(name = "make_price")
     @SerializedName("price") val preco: String,
+
+    @ColumnInfo(name="make_image")
     @SerializedName("image_link") val imagem: String,
 
     )
